2044310fbdc1b86b0803dc60aba7a7e7417da5d7
genshijin <vodka041619893616@outlook.jp>
2019-07-15 01:42:13 +0900

    fix to message_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,8 +11,10 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      format.html
-      format.json
+      respond_to do |format|
+        format.html { redirect_to "group_messages_path(params[:group_id])" }
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください'
