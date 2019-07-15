febeeda7bb22c3df6431334a02d3a7f1b78a3f9c
genshijin <vodka041619893616@outlook.jp>
2019-07-16 05:58:45 +0900

    Setting Incremental Search

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
+  def index
+  end
 
   def new
     @group = Group.new
@@ -15,6 +17,9 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+  end
+  
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
